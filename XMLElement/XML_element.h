c2374e462ccd18f87f60662544aa5a2211498fe1
Светослав Балтаджиев <baltadjiev@mail.bg>
2026-05-28 17:32:22 +0300

    Added XMLElement Class
    Wrote code for XML_element.h, and added XML_element.cpp

---
+++ XMLElement/XML_element.h
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <map>
+
+class XMLElement
+{
+    private:
+        std::string tagName;
+        std::string id;
+        std::string text;
+        std::map<std::string, std::string> attributes;
+        std::vector<XMLElement*> children;
+
+    public:
+        XMLElement(const std::string& name);
+        ~XMLElement();
+
+        //Капсулация за полетата на елемента
+        std::string getTagName() const;
+
+        std::string getId() const;
+        void setId(const std::string& id);
+
+        std::string getText() const;
+        void setText(const std::string& text);
+
+        //Управление на атрибутите на елемента
+        const std::map<std::string, std::string> getAttributes() const;
+        std::string getAttribute(const std::string& key) const;
+        void setAttribute(const std::string& key, const std::string& value);
+        void deleteAttribute(const std::string& key);
+
+        void addChild(XMLElement* child);
+        const std::vector<XMLElement*>& getChildren() const;
+};
