a79a42fb0965ed9d5b485a2c7feab39e19ed6b5a
Светослав Балтаджиев <baltadjiev@mail.bg>
2026-05-28 19:52:30 +0300

    Did the XMLEngine.h
    Now will work on XMLEngine.cpp

---
+++ XMLEngine/XMLEngine.h
@@ -0,0 +1,36 @@
+#include "XMLElement.h"
+#include <string>
+#include <map>
+
+class XMLEngine
+{
+    private:
+        std::string filePath;
+        XMLElement* root;
+
+        void clearTree();
+        void printElement(XMLElement* element, int depth) const;
+        XMLElement* findElementById(XMLElement* current, const std::string& targetId) const;
+
+    public:
+        XMLEngine();
+        ~XMLEngine();
+
+        void start();
+
+        void open(const std::string& filePath);
+        void close();
+        void save();
+        void saveAs(const std::string& savePath);
+        void help();
+
+        void print();
+        void selectAttribute(const std::string& id, const std::string& key);
+        void setAttribute(const std::string& id, const std::string& key, const std::string& value);
+        void deleteAttribute(const std::string& id, const std::string& key);
+        void showText(const std::string& id);
+        void printChildren(const std::string& id);
+        void printChild(const std::string& id, int n);
+        void addNewChild(const std::string& id);
+        void executeXPathQuery(const std::string& xPathQuery);
+};
